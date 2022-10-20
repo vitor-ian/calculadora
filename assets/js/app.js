d263c21a26488f96401634a0f6de256a763e7fba
Vítor Ian <82915119+vitor-ian@users.noreply.github.com>
2022-10-20 19:06:06 -0300

    Concluindo calculadora

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,58 @@
-function addValor(valor){
-    let visor = document.getElementById("visor").value;
-    let operador = "";
-
-    if(valor == 0 && visor == 0){
-        console.log(valor);
-        return false;
-    }else if(valor == "C"){
-        $("#visor").val("0");
-        return false;
-    }else if(valor == "%" || valor == "/" || valor == "x" || valor == "-" || valor == "+" ){
-        console.log("boa chefe");
-        operador = valor;
-        $("#visor").val("");
-    } 
-    else if(valor == "="){
-        resultado = valor01 + operador;
-        $("#visor").val(resultado);
-    }else{
-        $("#visor").val(valor);
+function writeNumber(elementId) {
+    var outputValueTo = document.getElementById('field1');
+  
+    if (outputValueTo.value == '0' || outputValueTo.value == 'Syntax error') {
+      outputValueTo.value = elementId.textContent;
+    } else {
+  
+      outputValueTo.value += elementId.textContent;
     }
-}
+  }
+  
+  function cleartxt() {
+    document.getElementById('field1').value = '0';
+    document.getElementById('dec').disabled = false;
+  }
+  
+  function setOperator(elementId) {
+    var outputValueTo = document.getElementById('field1');
+    if (outputValueTo.value == '0' || outputValueTo.value == 'Syntax error') {
+      outputValueTo.value = '0';
+    } else {
+      outputValueTo.value += elementId.textContent;
+      document.getElementById('dec').disabled = false;
+    }
+  }
+  
+  function setDecimal(elementId, status) {
+    var outputValueTo = document.getElementById('field1');
+    outputValueTo.value += elementId.textContent;
+    document.getElementById('dec').disabled = status;
+  }
+  
+  function calculate() {
+    try {
+  
+      var field1txt = document.getElementById('field1');
+      if (field1txt.value != '') {
+        var calculateResult = eval(field1txt.value);
+        field1txt.value = calculateResult;
+      }
+    } catch (err) {
+  
+      field1txt.value = 'Syntax error';
+  
+    }
+  
+  }
+  
+  function removeLastNumber() {
+    var field1txt = document.getElementById('field1');
+  
+    if (field1txt.value.length == 1 || field1txt.value == '0' || field1txt.value == 'Syntax error') {
+      field1txt.value = '0';
+      document.getElementById('dec').disabled = false;
+    } else {
+      field1txt.value = field1txt.value.substring(0, field1txt.value.length - 1);
+    }
+  }
